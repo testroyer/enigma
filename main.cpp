2c467dd1ca1651be42e04b2828a10fb7dd6e8283
testroyer <alperozer2007@gmail.com>
2026-03-10 23:53:41 +0300

    Remove character encryption and decryption examples from main function

--- main.cpp
+++ main.cpp
@@ -16,26 +16,6 @@ int main() {
         Plugboard plugboard = Plugboard(plugboardWiring);
         Enigma enigma = Enigma(rotors, reflector, plugboard);
 
-        // Encrypt char by char
-        char e1 = enigma.encrypt('A');
-        cout << "Encrypted A -> " << e1 << "\n";
-        cout << "Positions after: " << enigma.rotors[0].getPosition() << "," << enigma.rotors[1].getPosition() << "," << enigma.rotors[2].getPosition() << "\n";
-
-        char e2 = enigma.encrypt('Z');
-        cout << "Encrypted Z -> " << e2 << "\n";
-        cout << "Positions after: " << enigma.rotors[0].getPosition() << "," << enigma.rotors[1].getPosition() << "," << enigma.rotors[2].getPosition() << "\n";
-
-        // Reset and decrypt
-        enigma.setRotorPositions({0, 0, 0});
-        cout << "\n--- Reset ---\n\n";
-
-        char d1 = enigma.encrypt(e1);
-        cout << "Decrypted " << e1 << " -> " << d1 << "\n";
-        cout << "Positions after: " << enigma.rotors[0].getPosition() << "," << enigma.rotors[1].getPosition() << "," << enigma.rotors[2].getPosition() << "\n";
-
-        char d2 = enigma.encrypt(e2);
-        cout << "Decrypted " << e2 << " -> " << d2 << "\n";
-        cout << "Positions after: " << enigma.rotors[0].getPosition() << "," << enigma.rotors[1].getPosition() << "," << enigma.rotors[2].getPosition() << "\n";
 
     } catch (const std::exception& e) {
         cout << "exception: " << e.what() << "\n";
